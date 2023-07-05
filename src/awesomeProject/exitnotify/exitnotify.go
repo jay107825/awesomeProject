cc2d5e8a2902df107962772a0f790ad1c595a491
jay <17798128831@163.com>
2023-07-05 22:29:47 +0800

    避免在不必要的地方使用通道1.0

---
+++ src/awesomeProject/exitnotify/exitnotify.go
@@ -0,0 +1,9 @@
+package exitnotify
+
+/**
+ * @Author: admin
+ * @Description:
+ * @File: exitnotify
+ * @Version: 1.0.0
+ * @Date: 2023/7/5 22:12
+ */
---
+++ src/awesomeProject/overcreatechen/overcreatechen.go
@@ -0,0 +1,9 @@
+package overcreatechen
+
+/**
+ * @Author: admin
+ * @Description:
+ * @File: overcreatechen
+ * @Version: 1.0.0
+ * @Date: 2023/6/11 19:53
+ */
